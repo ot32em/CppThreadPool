75b37f679777d0d1389d944997aab8ca837b1050
OT Chen <ot32em@gmail.com>
2015-11-25 14:13:25 +0800

    test the invoker arguments copied or moved if std::move when created test the invokers arguments moved when called test the invoker executue the fun and return correctly test the invoker affect the outside value if passed by std::ref

--- projects/vs2015/test/test_invoker.cpp
+++ projects/vs2015/test/test_invoker.cpp
@@ -1,33 +1,114 @@
 #include "catch.hpp"
 #include "threadpool.h"
 #include <utility>
+#include <iostream>
 
-#define ASSERT_TUPLE_TYPE(f_, arg_, type_) std::is_same<std::tuple_element<0, decltype(make_invoker(f_, arg_))::TupleT>::type, type_>::value == true
+struct Observer
+{
+    static const bool verbose_ = false;
+    Observer(): moved_times_(0), copied_times_(0), default_created_times_(0){ }
+    void moved() {
+        if (verbose_) { std::cout << "moved\n"; }
+        moved_times_++;
+    }
+    void copied() {
+        if (verbose_) { std::cout << "copied\n"; }
+        copied_times_++;
+    }
+    void created() {
+        if (verbose_) { std::cout << "created\n"; }
+        default_created_times_++;
+    }
+private:
+    size_t moved_times_;
+    size_t copied_times_;
+    size_t default_created_times_;
+public:
+    size_t moved_times() const { return moved_times_; }
+    size_t copied_times() const { return copied_times_; }
+    size_t created_times() const { return default_created_times_; }
+};
 
-TEST_CASE("invoker tuple type (builtin type)", "[invoker][type]") {
-    auto func = [](int) {};
-    REQUIRE(ASSERT_TUPLE_TYPE(func, int(1), int));                          // pvalue
+struct ObservedObject
+{
+    ObservedObject(Observer& obs) : obs_(obs)
+    {
+        obs_.created();
+    }
+    ObservedObject(const ObservedObject& rhs) : obs_(rhs.obs_)
+    {
+        obs_.copied();
+    }
+    ObservedObject(ObservedObject&& rhs) : obs_(rhs.obs_)
+    {
+        obs_.moved();
+    }
+    Observer& obs_;
+};
 
-    int var = 1;
-    REQUIRE(ASSERT_TUPLE_TYPE(func, var, int));                             // lvalue
-    REQUIRE(ASSERT_TUPLE_TYPE(func, std::move(var), int));                  // xvalue
+TEST_CASE("invoker create, arguments are copied by value", "[invoker]") 
+{
+    auto funcByValue = [](ObservedObject) {};
+    Observer obs;
+    ObservedObject obj(obs);
+    auto invoker = make_invoker(funcByValue, obj);
+    REQUIRE(obs.copied_times() == 1);
+    REQUIRE(obs.moved_times() == 0);
 }
 
-TEST_CASE("invoker tuple type (dynamic type)", "[invoker][type]") {
-    auto func = [](std::string) {};
-    REQUIRE(ASSERT_TUPLE_TYPE(func, std::string("Hello"), std::string));    // pvalue
-
-    std::string var = "Hello";  
-    REQUIRE(ASSERT_TUPLE_TYPE(func, var, std::string));                     // lvalue
-    REQUIRE(ASSERT_TUPLE_TYPE(func, std::move(var), std::string));          // xvalue
+TEST_CASE("invoker create, arguments are moved to create if std::move", "[invoker]")
+{
+    auto funcByValue = [](ObservedObject) {};
+    Observer obs;
+    ObservedObject obj(obs);
+    auto invoker = make_invoker(funcByValue, std::move(obj));
+    REQUIRE(obs.copied_times() == 0);
+    REQUIRE(obs.moved_times() == 1);
 }
 
-TEST_CASE("invoker tuple type (c string type)", "[invoker][type]") {
-    auto func = [](std::string) {};
-    REQUIRE(ASSERT_TUPLE_TYPE(func, "Hello", const char*));         // pvalue
-
-    const char var[] = "Hello";
-    REQUIRE(ASSERT_TUPLE_TYPE(func, var, const char*));             // lvalue
-    REQUIRE(ASSERT_TUPLE_TYPE(func, std::move(var), const char*));  // xvalue
+TEST_CASE("invoker call, arguments are moved to call", "[invoker]") 
+{
+    auto funcByValue = [](ObservedObject) {};
+    Observer obs;
+    ObservedObject obj(obs);
+    auto invoker = make_invoker(funcByValue, obj);
+    auto m = obs.moved_times();
+    auto c = obs.copied_times();
+    invoker();
+    auto m_times = obs.moved_times() - m;
+    auto c_times = obs.copied_times() - c;
+    REQUIRE(m_times == 1);
+    REQUIRE(c_times == 0);
 }
 
+TEST_CASE("invoker call, return the right value", "[invoker]")
+{
+    {
+        auto add2 = [](int a, int b) { return a + b; };
+        auto invoker = make_invoker(add2, 1, 2);
+        REQUIRE(invoker() == add2(1, 2));
+    }
+    {
+        auto concat = [](std::string a, std::string b) { return a + b; };
+        auto invoker = make_invoker(concat, "Hello ", "World");
+        REQUIRE(invoker() == concat("Hello ", "World"));
+    }
+}
+
+TEST_CASE("invoker call, actually affected by reference", "[invoker]")
+{
+    {
+        auto inc = [](int& a) { a++; };
+        int a = 100;
+        auto invoker = make_invoker(inc, std::ref(a));
+        invoker();
+        REQUIRE(a == 101);
+    }
+    {
+        auto sayto = [](std::string& a) { a += " World"; };
+        std::string a = "Hello";
+        auto invoker = make_invoker(sayto, std::ref(a));
+        invoker();
+        REQUIRE(a == "Hello World");
+    }
+}
